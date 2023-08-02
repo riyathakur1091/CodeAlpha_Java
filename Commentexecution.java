04b410d677c0e00e81f146518f49c797bae27923
riyathakur1091 <93506185+riyathakur1091@users.noreply.github.com>
2023-08-02 20:27:35 +0530

    Add files via upload

---
+++ Commentexecution.java
@@ -0,0 +1,9 @@
+public class Commentexecution {
+    public static void main(String[] args) {
+        int alpha = 10;
+
+        // I'm changing the value of \u000d alpha = 30;
+
+        System.out.println("Value of alpha : " + alpha);
+    }
+}
