1b28098bd7ccb5145d5f8620548ca687f5fd8f81
riyathakur1091 <93506185+riyathakur1091@users.noreply.github.com>
2023-08-02 20:20:54 +0530

    Add files via upload

---
+++ Infinite_loop.java
@@ -0,0 +1,16 @@
+public class Infinite_loop {
+    public static void main(String[] args) {
+        // Infinite loop
+        while (true) {
+            // Code inside the loop will keep executing indefinitely
+            System.out.println("This is an infinite loop!");
+        }
+
+        
+    }
+}
+
+
+
+
+
