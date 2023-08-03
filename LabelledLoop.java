b1832f8a28a0ada41eef8e0b5121c0c411fb5af5
riyathakur1091 <93506185+riyathakur1091@users.noreply.github.com>
2023-08-03 11:25:59 +0530

    Add files via upload

---
+++ LabelledLoop.java
@@ -0,0 +1,17 @@
+class LabelledLoop
+    {
+        public static void main(String args[])
+        {
+            int i,j;
+            loop1:   for(i=1;i<=10;i++)
+            {
+                System.out.println();
+                loop2:   for(j=1;j<=10;j++)
+                {
+                    System.out.print(j + " ");
+                    if(j==5)
+                        break loop1;     //Statement 1
+                }
+            }
+        }
+    }
